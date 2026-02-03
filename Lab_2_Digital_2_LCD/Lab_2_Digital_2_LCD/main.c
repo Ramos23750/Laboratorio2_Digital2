f8fc692bdb52256e913d5e618a0e4ff916b4705d
Ramos23750 <ram23750@uvg.edu.gt>
2026-02-02 22:42:27 -0600

    ADC interrups
    Terminando la funcion de los potenciometros

--- Lab_2_Digital_2_LCD/Lab_2_Digital_2_LCD/main.c
+++ Lab_2_Digital_2_LCD/Lab_2_Digital_2_LCD/main.c
@@ -18,24 +18,64 @@
 #include "UART/UART.h"
 /********************* Variables ****************************/
 float pot1,pot2; //Variables donde se guardaran los valores 
+uint8_t adc_value;
+uint16_t conv1,conv2;
+uint8_t alternador;
+char buff1[7], buff2[7];
 /********************  Function Prototype *****************************/
-
 /********************** Main Loop ***************************/
 int main(void)
 {
+	init_ADC();
 	init8bits();
-	LCD_SET_CURSOR(1,1);
+	LCD_SET_CURSOR(2,1);
+	LCD_WRITE_STRING("S1:");
+	LCD_SET_CURSOR(8,1);
+	LCD_WRITE_STRING("S2:");
+	LCD_SET_CURSOR(14,1);
+	LCD_WRITE_STRING("S3:");
+	LCD_SET_CURSOR(13,2);
+	LCD_WRITE_STRING("C: ");
 	
 	while (1)
 	{
-		
+		ADCSRA |= (1 << ADSC);						
+		LCD_SET_CURSOR(1,2);
+		LCD_WRITE_STRING(buff1);														
+		LCD_SET_CURSOR(7,2);
+		LCD_WRITE_STRING(buff2);															
 	}
 }
 
-/******************* Non-interrupt functions ******************************/
-
-
 
 /******************** Interrupt functions ****************************/
-
-///////////////////////////Pruebas
+ISR(ADC_vect)
+{
+	adc_value = ADCH;
+	switch(alternador)
+	{
+		case 0:
+		pot1 = adc_value;
+		conv1 = (pot1 * 5000 / 255);
+		snprintf(buff1, sizeof(buff1), "%u.%02uV", conv1/1000, (conv1 % 1000) / 10);
+		ADMUX = 0;
+		ADMUX |= (1 << REFS0);												
+		ADMUX |= (1 << ADLAR);												
+		ADMUX |= (1 << MUX1);	
+		alternador = 1;										
+		break;
+		
+		case 1:
+		pot2 = adc_value;
+		conv2 = (pot2 * 5000 / 255);
+		snprintf(buff2, sizeof(buff2), "%u.%02uV", conv2/1000, (conv2 % 1000) / 10);
+		ADMUX = 0;
+		ADMUX |= (1 << REFS0);										
+		ADMUX |= (1 << ADLAR);												
+		ADMUX |= (1 << MUX0);
+		alternador = 0;												
+		break;
+		default:
+		break;
+	}
+}
